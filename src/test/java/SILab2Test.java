7aeed2ffe5675502562e4aa4d3003b7e4b28101f
finki <andrej.shekerov@students.finki.ukim.mk>
2023-05-29 13:46:16 +0200

    Add one simple test as an example.

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,5 +1,36 @@
-//package src.test.java;
+import org.junit.jupiter.api.Test;
 
-public class SILab2Test {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    private List <User> createList( User ... elems ) {
+        return new ArrayList<>( Arrays.asList(elems) );
+    }
+
+    @Test
+    void SimpleTest(){
+
+        User null_user = null;
+        List <User> user_list = createList();
+
+        boolean caught_correct_exception = false;
+        String expected_message = "Mandatory information missing!";
+
+        try{
+            SILab2.function( null_user, user_list );
+        } catch ( RuntimeException x ){
+            if( x.getMessage().equals( expected_message ) ){
+                caught_correct_exception = true;
+            }
+        }
+
+        assertTrue( caught_correct_exception );
+
+    }
 
 }
